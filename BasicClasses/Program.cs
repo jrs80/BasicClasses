7271775088e289f576ac87857d2c93d23ccfafd3
JRS80 <jason.steffen@gmail.com>
2022-06-13 06:52:56 -0500

    basic classes exercise

---
+++ BasicClasses/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Car
+{
+    public string Make;
+    public string Model;
+    public int Year;
+}
+namespace BasicClasses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car() { Make = "Honda", Model = "Odyssey", Year = 2014 };
+            Console.WriteLine("Car information:");
+            Console.WriteLine("Make: " + car.Make);
+            Console.WriteLine("Model:" + car.Model);
+            Console.WriteLine("Year: " + car.Year);
+            Console.ReadKey();
+        }
+    }
+}
